b4c138ba77fb72eb14035b0269e2948cab20de2f
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-12-20 22:54:11 +0600

    Project created

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+// Created by MD ABIR HOSSAIN on 25-12-2023 at 6:34 PM
+// Website: https://mdabirhossain.com/
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
